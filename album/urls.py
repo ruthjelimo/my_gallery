1eb2671db7a9f7ff08970a2fa4765ee851b4ea12
ruthmimo <ruthjmimo@gmail.com>
2022-03-25 21:40:51 +0300

    added view file

--- album/urls.py
+++ album/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path,re-path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
     path('today/',views.album_of_day,name='albumToday'),
-    re-path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_album,name = 'pastAlbum') 
+    re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_album,name = 'pastAlbum') 
 ]
--- album/views.py
+++ album/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Ruth gallery site')
+    return render(request, 'welcome.html')
+    # return HttpResponse('Ruth gallery site')
 
 def album_of_day(request):
     date = dt.date.today()
@@ -34,6 +35,24 @@ def album_of_day(request):
     date = dt.date.today()
 
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Album for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+def past_days_album(request,past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()   
+
     day = convert_dates(date)
     html = f'''
         <html>

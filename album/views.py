06246dcae91843a9d7b7acaa4bbc77b5c1e8510f
ruthmimo <ruthjmimo@gmail.com>
2022-03-25 21:06:17 +0300

    added files to view

--- album/views.py
+++ album/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Ruth gallery site)
+    return HttpResponse('Ruth gallery site')
+
+def album_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
 
 # Create your views here.

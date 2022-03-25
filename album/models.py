bf9939038c6de490a649aa779ba45ff15d8d32bc
ruthmimo <ruthjmimo@gmail.com>
2022-03-25 23:42:55 +0300

    updated models

--- album/models.py
+++ album/models.py
@@ -7,3 +7,6 @@ class Author(models.Model):
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
     phone_number = models.CharField(max_length=300, blank=True)
+    
+    def __str__(self):
+        return self.first_name
--- album/views.py
+++ album/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
+from .models import Author,Picture,Category,Location
 import datetime as dt
 
 # Create your views here.

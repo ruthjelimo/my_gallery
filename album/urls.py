19b33a1dfc12737b63119cef6f71ff7b5cb65a70
ruthmimo <ruthjmimo@gmail.com>
2022-03-26 20:52:54 +0300

    updated url file

--- album/urls.py
+++ album/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path,re_path
+
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns=[
-    path('',views.welcome,name = 'welcome'),
-    path('today/',views.album_of_day,name='albumToday'),
-    re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_album,name = 'pastAlbum') 
-]
+    path('',views.images,name='images'),
+    path('single_image/(\d+)',views.single_image,name='single_image'),
+    path('search/',views.search_results,name = 'search_results'),
+    path('location/(\d+)',views.viewImg_by_location,name='locationimage'),
+    path('category/(\d+)',views.viewImg_by_category, name = 'categoryimage')
+    ]

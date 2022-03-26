b5bc2cc3b31b04e827fe7403ef248846783b4cf1
ruthmimo <ruthjmimo@gmail.com>
2022-03-26 11:32:30 +0300

    added Image class

--- album/models.py
+++ album/models.py
@@ -7,6 +7,19 @@ class Author(models.Model):
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
     phone_number = models.CharField(max_length=300, blank=True)
-    
+
     def __str__(self):
         return self.first_name
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to= 'images/')
+    description = models.CharField(max_length=50)
+    name = models.CharField(max_length=55)    
+    date = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey('Location', on_delete = models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    author = models.ForeignKey(Author,on_delete = models.CASCADE)
+
+   
+

fe5bc52cfe4f3dbe2566361e8b886e1791081abd
ruthmimo <ruthjmimo@gmail.com>
2022-03-26 13:09:25 +0300

    updated model file

--- album/models.py
+++ album/models.py
@@ -16,10 +16,47 @@ class Image(models.Model):
     image = models.ImageField(upload_to= 'images/')
     description = models.CharField(max_length=50)
     name = models.CharField(max_length=55)    
-    date = models.DateTimeField(auto_now_add=True)
-    location = models.ForeignKey('Location', on_delete = models.CASCADE)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+   
+    # location = models.ForeignKey('Location', on_delete = models.CASCADE)
+    # category = models.ForeignKey('Category', on_delete=models.CASCADE)
     author = models.ForeignKey(Author,on_delete = models.CASCADE)
+
+
+    def save_image(self):
+        self.save()                    
+    
+    def delete_image(self):
+        self.delete()
+  
+    @classmethod
+    def all_images(cls):
+        images = cls.objects.all()
+        return images
+
+    @classmethod
+    def get_one_image(cls,id):
+        images = cls.objects.filter(id = id)
+        return Image
+
+    @classmethod
+    def search_by_name(cls,search_term):
+        image = cls.objects.filter(name__icontains=search_term)
+        return image
+
+    
+    @classmethod
+    def view_images_by_location(cls,location):
+        location_images = cls.objects.filter(location= location)
+
+        return location_images
+
+    @classmethod
+    def view_images_by_category(cls,category):
+        category = cls.objects.filter(category = category)
+        return category
+
+    
 
    
 
--- album/tests.py
+++ album/tests.py
@@ -2,3 +2,14 @@ from django.test import TestCase
 from .models import Author,Image
 
 # Create your tests here.
+class AuthorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        
+        self.Kimberly=Author(first_name = 'Kimberly',last_name = 'Kardashian',email='kimkay@gmail.com',phone_number='0712437151')
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Kimberly,Author))
+
+
